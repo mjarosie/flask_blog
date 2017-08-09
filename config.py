915f3f40637f1e49283f95f0d41a400bc7dde0af
mjarosie <jarosiewicz.maciek@gmail.com>
2017-08-09 21:57:33 +0100

    Added a scribt that creates the database. Fixed typo in config.py

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import os
 
+
 class Configuration(object):
     APPLICATION_DIR = os.path.dirname(os.path.realpath(__file__))
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:////{}/blog.db'.format(APPLICATION_DIR)
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///{}/blog.db'.format(APPLICATION_DIR)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
