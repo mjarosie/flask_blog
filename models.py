00b7d6ce5f93c15082f67037f532c63ea69e484e
mjarosie <jarosiewicz.maciek@gmail.com>
2017-08-09 23:02:28 +0100

    Added Tag model to models.py

--- models.py
+++ models.py
@@ -1,7 +1,6 @@
 import datetime, re
 from app import db
 
-
 def slugify(s):
     return re.sub('[^\w]+', '-', s).lower()
 
@@ -25,3 +24,16 @@ class Entry(db.Model):
 
     def __repr__(self):
         return '<Entry: {}>'.format(self.title)
+
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))
+    slug = db.Column(db.String(64), unique=True)
+
+    def __init(self, *args, **kwargs):
+        super(Tag, self).__init__(*args, **kwargs)
+        self.slug = slugify(self.name)
+
+    def __repr__(self):
+        return '<Tag {}>'.format(self.name)
