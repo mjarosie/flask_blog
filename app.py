35e83d305c83cee4b1ec9ca95a10626f995c4eda
mjarosie <jarosiewicz.maciek@gmail.com>
2017-08-09 21:23:29 +0100

    Added SQLite database connection

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from config import Configuration # import our configuration data.
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config.from_object(Configuration) # use values from our Configuration object.
+app.config.from_object(Configuration)  # use values from our Configuration object.
+db = SQLAlchemy(app)
--- config.py
+++ config.py
@@ -1,2 +1,7 @@
+import os
+
 class Configuration(object):
+    APPLICATION_DIR = os.path.dirname(os.path.realpath(__file__))
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:////{}/blog.db'.format(APPLICATION_DIR)
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
