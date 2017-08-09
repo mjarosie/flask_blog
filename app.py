ef32a813a30b111b19a22f4676c6fe1411e50362
mjarosie <jarosiewicz.maciek@gmail.com>
2017-08-09 23:43:05 +0100

    Added possibility of migration of database

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask
-from config import Configuration # import our configuration data.
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
 from flask_sqlalchemy import SQLAlchemy
+
+from config import Configuration  # import configuration data.
 
 app = Flask(__name__)
 app.config.from_object(Configuration)  # use values from our Configuration object.
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
