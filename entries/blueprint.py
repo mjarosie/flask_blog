64f742bd1c5d0a94ea9fc70538a25f2367dc7343
mjarosie <jarosiewicz.maciek@gmail.com>
2017-08-11 19:19:46 +0100

    Added an blog entry view

--- entries/blueprint.py
+++ entries/blueprint.py
@@ -1,22 +1,27 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 from helpers import object_list
 from models import Entry
 
 entries = Blueprint('entries', __name__, template_folder='templates')
+
 
 @entries.route('/')
 def index():
     entries = Entry.query.order_by(Entry.created_timestamp.desc())
     return object_list('entries/index.html', entries)
 
+
 @entries.route('/tags/')
 def tag_index():
     pass
 
+
 @entries.route('/tags/<slug>/')
-def tag_details(slug):
+def tag_detail(slug):
     pass
+
 
 @entries.route('/<slug>/')
 def detail(slug):
-    pass
+    entry = Entry.query.filter(Entry.slug == slug).first_or_404()
+    return render_template('entries/detail.html', entry=entry)
